fee14901123442946c22c9dcf8c2eac62c4989b9
Danish Mehmood <danish.mehmood@otif-solutions.com>
2022-07-16 21:55:34 +0500

    minor changes

--- routes/web.php
+++ routes/web.php
@@ -4,13 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
 
 
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-
-
 Route::get('login', [AdminController::class, 'login']);
 Route::post('login', [AdminController::class, 'login']);
 
