3984182858137a947ded3307c26b53a724cc2167
Danish Mehmood <danish.mehmood@otif-solutions.com>
2022-07-16 23:19:37 +0500

    migrate the basic login routes to laravel 9 routes

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,13 @@ use App\Http\Controllers\AdminController;
 
 
 
+// Route::get('login', [AdminController::class, 'login']);
+// Route::post('login', [AdminController::class, 'login']);
+// Route::get('home', [AdminController::class, 'dashboard']);
 
-Route::get('login', [AdminController::class, 'login']);
-Route::post('login', [AdminController::class, 'login']);
+
+Route::controller(AdminController::class)->group(function() {
+    Route::get('login', 'login');
+    Route::post('login', 'login');
+    Route::get('home', 'dashboard');
+});
