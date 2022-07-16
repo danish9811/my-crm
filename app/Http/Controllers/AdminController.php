d439beda19db3536429c41004fdb6b21aa49dc0e
Danish Mehmood <danish.mehmood@otif-solutions.com>
2022-07-16 22:14:30 +0500

    minor change

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -5,15 +5,13 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller {
+
     public function login(Request $request) {
-        return "hello world";
-        // validateions
-        return redirect('login');
+        return view('login');
     }
 
     public function home() {
-        return "hello world";
-        // return redirect('home')->withErrors('Username or password is incorrect');
+        return view('dashboard');
     }
 
 }
