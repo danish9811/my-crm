200fe52494c5be40e0fe583ea3e8901afc2a2dbb
Danish Mehmood <danish.mehmood@otif-solutions.com>
2022-07-17 01:04:12 +0500

    tiny change

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
 
-
-
-// Route::get('login', [AdminController::class, 'login']);
-// Route::post('login', [AdminController::class, 'login']);
-// Route::get('home', [AdminController::class, 'dashboard']);
-
-
 Route::controller(AdminController::class)->group(function() {
     Route::get('/login', 'login');
     Route::post('/login', 'login');
