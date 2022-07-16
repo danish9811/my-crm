af41556fa9dbe223ee1b3e7b569ceeb15d8a7b7b
Danish Mehmood <danish.mehmood@otif-solutions.com>
2022-07-17 00:34:05 +0500

    tiny changes bla bla bla

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,14 +3,36 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class AdminController extends Controller {
 
     public function login(Request $request) {
-        return view('login');
+        $submit = $request['submit'];
+
+        // if submit key has "submit" value, then do this, it means it is a post route, post request
+        // if the submti has the value of "submit", its the attribute value that is set in html submit button
+        if ($submit == 'submit') {
+            // we have set no validation on html , but all the validation server side,
+            // https://laravel.com/docs/9.x/authentication#authenticating-users
+            $request->validate([
+                'email' => 'required',
+                'password' => 'required'
+            ]);
+
+            if (\Auth::attempt($request->only('email', 'password'))) {
+                return redirect('/home');
+            } else {
+                return redirect('/login')->withError('Username or Password is incorrect');
+            }
+        }
+
+        // if submit is not clicked, then do this, it means it will called if request is get
+        // return view('login');
     }
 
-    public function home() {
+    public function dashboard() {
+        return 'dashboard';
         return view('dashboard');
     }
 
