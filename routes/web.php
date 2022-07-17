105a1b509daa05c72a95ca22e6a45af31a2930c8
Danish Mehmood <danish.mehmood@otif-solutions.com>
2022-07-17 18:20:24 +0500

    added routes and prefix for leads

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,12 @@ Route::controller(AdminController::class)->group(static function() {
     Route::group(['middleware' => 'auth'], static function() {
         Route::get('/home', 'dashboard');
         Route::get('/logout', 'logout');
+
+        Route::group(['prefix' => 'leads'], static function() {
+            Route::get('/add-lead', 'addLead');
+            Route::get('/manage-leads', 'manageLeads');
+        });
+
     });
 
 });
