352c736c4e45e92dcb5bcedc61348d189177d9b0
Danish Mehmood <danish.mehmood@otif-solutions.com>
2022-07-17 19:04:42 +0500

    formatted html for add lead page, formatted inputs

--- resources/views/leads/add_lead.blade.php
+++ resources/views/leads/add_lead.blade.php
@@ -18,64 +18,108 @@
           <div class="card-header bg-blue">
             <h5 class="text-white m-b-0">Lead Information</h5>
           </div>
+
+          <!-- card body start -->
           <div class="card-body">
-            <form>
+
+
+            <form method="post" action="">
+              @csrf
+
               <div class="row">
+
+                <!-- single colomn for first anme -->
                 <div class="col-md-6">
                   <div class="form-group has-feedback">
                     <label class="control-label">First Name</label>
                     <input class="form-control" placeholder="First Name" type="text">
-                    <span class="fa fa-user form-control-feedback" aria-hidden="true"></span> </div>
-                  </div>
-                  <div class="col-md-6">
-                    <div class="form-group has-feedback">
-                      <label class="control-label">Last Name</label>
-                      <input class="form-control" placeholder="Last Name" type="text">
-                      <span class="fa fa-user form-control-feedback" aria-hidden="true"></span> </div>
-                    </div>
-                    <div class="col-md-6">
-                      <div class="form-group has-feedback">
-                        <label class="control-label">E-mail</label>
-                        <input class="form-control" placeholder="E-mail" type="text">
-                        <span class="fa fa-envelope-o form-control-feedback" aria-hidden="true"></span> </div>
-                      </div>
-                      <div class="col-md-6">
-                        <div class="form-group has-feedback">
-                          <label class="control-label">Contact Number</label>
-                          <input class="form-control" placeholder="Contact Number" type="text">
-                          <span class="fa fa-phone form-control-feedback" aria-hidden="true"></span> </div>
-                        </div>
-                        <div class="col-md-6">
-                          <div class="form-group has-feedback">
-                            <label class="control-label">Company</label>
-                            <input class="form-control" placeholder="Company" type="text">
-                            <span class="fa fa-briefcase form-control-feedback" aria-hidden="true"></span> </div>
-                          </div>
-                          <div class="col-md-6">
-                            <div class="form-group has-feedback">
-                              <label class="control-label">Website</label>
-                              <input class="form-control" placeholder="https://" type="text">
-                              <span class="fa fa-globe form-control-feedback" aria-hidden="true"></span> </div>
-                            </div>
-                            <div class="col-md-12">
-                              <div class="form-group has-feedback">
-                                <label class="control-label">Bio</label>
-                                <textarea class="form-control" id="placeTextarea" rows="3" placeholder="Bio"></textarea>
-                              </div>
-                            </div>
-                            <div class="col-md-12">
-
-                              <div class="col-md-12">
-                                <button type="submit" class="btn btn-success">Submit</button>
-                              </div>
-                            </div>
-                          </form>
-                        </div>
-                      </div>
-                    </div>
+                    <span class="fa fa-user form-control-feedback" aria-hidden="true"></span>
                   </div>
                 </div>
-                <!-- /.content -->
-              </div>
-              <!-- /.content-wrapper -->
-              @endsection
+
+                <!-- single colomn for last name -->
+                <div class="col-md-6">
+                  <div class="form-group has-feedback">
+                    <label class="control-label">Last Name</label>
+                    <input class="form-control" placeholder="Last Name" type="text">
+                    <span class="fa fa-user form-control-feedback" aria-hidden="true"></span>
+                  </div>
+                </div>
+
+                <!-- single colomn for email -->
+                <div class="col-md-6">
+                  <div class="form-group has-feedback">
+                    <label class="control-label">E-mail</label>
+                    <input class="form-control" placeholder="E-mail" type="text">
+                    <span class="fa fa-envelope-o form-control-feedback" aria-hidden="true"></span>
+                  </div>
+                </div>
+
+                <!-- single colomn for number -->
+                <div class="col-md-6">
+                  <div class="form-group has-feedback">
+                    <label class="control-label">Contact Number</label>
+                    <input class="form-control" placeholder="Contact Number" type="text">
+                    <span class="fa fa-phone form-control-feedback" aria-hidden="true"></span>
+                  </div>
+                </div>
+
+                <!-- single colomn for company -->
+                <div class="col-md-6">
+                  <div class="form-group has-feedback">
+                    <label class="control-label">Company</label>
+                    <input class="form-control" placeholder="Company" type="text">
+                    <span class="fa fa-briefcase form-control-feedback" aria-hidden="true"></span>
+                  </div>
+                </div>
+
+                <!-- drop down single -->
+                <div class="col-md-6">
+                  <div class="form-group has-feedback">
+                    <label class="control-label">Lead Source</label>
+                    <select class="form-control" name="" id="">
+                      <option value="">1</option>
+                      <option value="">2</option>
+                      <option value="">3</option>
+                      <option value="">4</option>
+                      <option value="">5</option>
+                    </select>
+                    <span class="fa fa-globe form-control-feedback" aria-hidden="true"></span>
+                  </div>
+                </div>
+
+                <!-- single field -->
+                <div class="col-md-6">
+                  <div class="form-group has-feedback">
+                    <label class="control-label">Website</label>
+                    <input class="form-control" placeholder="https://" type="text">
+                    <span class="fa fa-globe form-control-feedback" aria-hidden="true"></span>
+                  </div>
+                </div>
+
+                <!-- text area -->
+                <div class="col-md-12">
+                  <div class="form-group has-feedback">
+                    <label class="control-label">Bio</label>
+                    <textarea class="form-control" id="placeTextarea" rows="3" placeholder="Bio"></textarea>
+                  </div>
+                </div>
+
+                <!-- submit button -->
+                <div class="col-md-12">
+                  <div class="col-md-12">
+                    <button type="submit" class="btn btn-success">Submit</button>
+                  </div>
+                </div>
+                <!-- end submit button -->
+
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+  @endsection
