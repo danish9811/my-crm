121b4a36ae4afe44b8778300948b546cb5dc537c
Danish Mehmood <danish.mehmood@otif-solutions.com>
2022-07-17 13:58:44 +0500

    added route for logout and name login route

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,14 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
 
-Route::controller(AdminController::class)->group(function() {
-    Route::get('/login', 'login');
+Route::controller(AdminController::class)->group(static function() {
+
+    Route::get('/login', 'login')->name('login');
     Route::post('/login', 'login');
-    Route::get('/home', 'dashboard');
+
+    Route::group(['middleware' => 'auth'], static function() {
+        Route::get('/home', 'dashboard');
+        Route::get('/logout', 'logout');
+    });
+
 });
