2c41683ce667ddb94c6714eb82a28705cf7afaf3
Danish Mehmood <danish.mehmood@otif-solutions.com>
2022-07-25 00:09:45 +0500

    madde the lead migrateion more clean and readable

--- database/migrations/2022_07_24_170936_create_leads_table.php
+++ database/migrations/2022_07_24_170936_create_leads_table.php
@@ -4,18 +4,10 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('leads', function (Blueprint $table) {
+return new class extends Migration {
 
-            // if the table does not have relation with another table, make it MyIsam
+    public function up() {
+        Schema::create('leads', function (Blueprint $table) {
             $table->engine = 'myIsam';
             $table->id();
             $table->string('first_name', 100);
@@ -36,13 +28,7 @@ return new class extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
+    public function down() {
         Schema::dropIfExists('leads');
     }
 };
