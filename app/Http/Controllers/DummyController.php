25531899187bb4484cf5d8b14cf00f1f63bb959b
danish mehmood <danish.mehmood@otif-solutions.com>
2022-09-21 11:15:10 +0500

    Spaces added that are faltu

--- app/Http/Controllers/DummyController.php
+++ app/Http/Controllers/DummyController.php
@@ -2,11 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Dummy;
+
 class DummyController extends Controller {
 
     public function index() {
         return "Hello from the method : " . __METHOD__;
     }
+
+
+
+
 
 
 }
