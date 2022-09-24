21d01dabf0955b1ae77f432b81b40d32468faa5a
danish mehmood <danish.mehmood@otif-solutions.com>
2022-09-24 20:06:26 +0500

    Way of definining routes changed to laravel 8 for simplicity and make phpstorm understand them

--- routes/web.php
+++ routes/web.php
@@ -9,25 +9,49 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/dummy', [DummyController::class, 'dummy']);    // create DummyController with dummy model too
 
 // controller group for laravel 9 route
-Route::controller(AdminController::class)->group(static function () {
 
-    Route::get('/default', 'defaultMethod');
+// laravel 9 routes
+//Route::controller(AdminController::class)->group(static function () {
+//
+//    Route::get('/default', 'defaultMethod');
+//
+//    Route::get('/register', 'register');
+//    Route::get('/login', 'login')->name('login');
+//    Route::post('/login', 'login');
+//
+//    Route::group(['middleware' => 'auth'], static function () {
+//        Route::get('/home', 'dashboard');
+//        Route::get('/logout', 'logout');
+//
+//        Route::group(['prefix' => 'leads'], static function () {
+//            Route::get('/add-lead', 'addLead');
+//            Route::post('/add-lead', 'addLead');
+//            Route::get('/manage-leads', 'manageLeads');
+//            Route::get('/delete-lead/{id}', 'deleteLead');
+//            Route::get('/edit-lead/{id}', 'editLead');
+//            Route::post('/edit-lead/{id}', 'editLead');
+//        });
+//    });
+//});
 
-    Route::get('/register', 'register');
-    Route::get('/login', 'login')->name('login');
-    Route::post('/login', 'login');
+Route::get('/register', [AdminController::class, 'register']);
+Route::get('/login', [AdminController::class, 'login'])->name('login');
+Route::post('/login', [AdminController::class, 'login']);
 
-    Route::group(['middleware' => 'auth'], static function () {
-        Route::get('/home', 'dashboard');
-        Route::get('/logout', 'logout');
+Route::get('/default', [AdminController::class, 'defaultMethod']);
 
-        Route::group(['prefix' => 'leads'], static function () {
-            Route::get('/add-lead', 'addLead');
-            Route::post('/add-lead', 'addLead');
-            Route::get('/manage-leads', 'manageLeads');
-            Route::get('/delete-lead/{id}', 'deleteLead');
-            Route::get('/edit-lead/{id}', 'editLead');
-            Route::post('/edit-lead/{id}', 'editLead');
-        });
+Route::group(['middleware' => 'auth'], static function () {
+    Route::get('/home', [AdminController::class, 'dashboard']);
+    Route::get('/logout', [AdminController::class, 'logout']);
+    Route::group(['prefix' => 'leads'], static function () {
+        Route::get('/add-lead', [AdminController::class, 'addLead']);
+        Route::post('/add-lead', [AdminController::class, 'addLead']);
+        Route::get('/manage-leads', [AdminController::class, 'manageLeads']);
+        Route::get('/delete-lead/{id}', [AdminController::class, 'deleteLead']);
+        Route::get('/edit-lead/{id}', [AdminController::class, 'editLead']);
+        Route::post('/edit-lead/{id}', [AdminController::class, 'editLead']);
     });
 });
+
+
+
