fd5a987237c346dae40be940a43a19a44379d225
danish mehmood <danish.mehmood@otif-solutions.com>
2022-09-26 04:11:58 +0500

    cleaned code

--- app/Models/Lead.php
+++ app/Models/Lead.php
@@ -9,8 +9,6 @@ class Lead extends Model {
 
     use HasFactory;
 
-    protected $table = 'leads';     // if we are not following laravel conventions
-    protected $guarded = ['id'];    // mass assignment except 'id'
-    protected $primaryKey = 'id';   // if we explicityly define the primary key, and we don't follow the convensions
+    protected $guarded = ['id'];
 
 }
